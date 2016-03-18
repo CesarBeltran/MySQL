00fd68b897472c6c73db297fcf29ca4a0a044a18
Cesar Ramiro Beltrán Hernandez <cesar-beltran-85@hotmail.es>
2016-03-17 22:50:59 -0500

    verificacion de conexion con servidor Local Host
    s

--- mis-contactos/pruebas-bd.php
+++ mis-contactos/pruebas-bd.php
@@ -3,7 +3,7 @@
 1)Conectarme a la BD. mysql necesita 3 datos:
 		-Servidor, Usuario de la BD, Password del usuario
 */
-$conexion = mysql_connect("localhost","root","")or die("No se pudo conectar con el servidor de BD");
+$conexion = mysql_connect("localhost","root","cesar")or die("No se pudo conectar con el servidor de BD");
 echo "Se ha podido realizar cone con MySQL<br/>";
 
 ?>
